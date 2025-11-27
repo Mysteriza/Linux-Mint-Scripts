b3a49d19fae1ee520e191e8e078b21601266b96f
Riza <mysteriza@proton.me>
2025-11-27 12:20:16 +0700

    Update check_honeygain_status script to set default container name and increase log lines displayed

--- check_honeygain_status.sh
+++ check_honeygain_status.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-CONTAINER_NAME=""
+CONTAINER_NAME="charming_aryabhata"
 
 echo "--- Honeygain Container Status ---"
 docker ps -a --filter name=$CONTAINER_NAME --format "table {{.Names}}\t{{.Status}}\t{{.ID}}"
 
-echo $'\n--- Last 5 Log Lines (WIB) ---'
-docker logs $CONTAINER_NAME --tail 5 --timestamps 2>&1 | while read -r timestamp message; do
+echo $'\n--- Last 10 Log Lines (WIB) ---'
+docker logs $CONTAINER_NAME --tail 10 --timestamps 2>&1 | while read -r timestamp message; do
     formatted_date=$(date -d "$timestamp" "+%Y-%m-%d %H:%M:%S")
     echo "$formatted_date | $message"
 done
